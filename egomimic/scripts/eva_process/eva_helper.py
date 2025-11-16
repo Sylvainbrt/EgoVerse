06f30d96fb453ea02f3693593b03a7cb231d363a
Ryan Punamiya <38972198+ryanthecreator@users.noreply.github.com>
2025-11-15 19:27:11 -0500

    Update eva_helper.py

--- egomimic/scripts/eva_process/eva_helper.py
+++ egomimic/scripts/eva_process/eva_helper.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from types import SimpleNamespace
+
 from egomimic.scripts.eva_process.eva_to_lerobot import main as eva_main
+
 
 def lerobot_job(
     *,
@@ -9,7 +11,7 @@ def lerobot_job(
     dataset_name: str,
     arm: str,
     description: str = "",
-    extrinsics: dict | list | None = None,
+    extrinsics_key: str = "ariaOct18_arx",
 ) -> None:
     raw_path = Path(raw_path).expanduser().resolve()
     output_dir = Path(output_dir).expanduser().resolve()
@@ -20,18 +22,19 @@ def lerobot_job(
         dataset_repo_id=f"rpuns/{dataset_name}",
         fps=50,
         arm=arm,
+        extrinsics_key=extrinsics_key,
         description=description,
         image_compressed=False,
         video_encoding=False,
         prestack=True,
+        nproc=12,
+        nthreads=2,
         output_dir=output_dir,
         push=False,
         private=False,
         license="apache-2.0",
-        nproc=12,
-        nthreads=2,
         debug=False,
-        extrinsics_key=extrinsics,
+        save_mp4=True,
     )
 
     eva_main(args)
